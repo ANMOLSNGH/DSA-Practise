2a056136cc8ea93889b2392cd10a55a315480ed8
ANMOLSNGH <101090890+ANMOLSNGH@users.noreply.github.com>
2026-07-26 15:27:32 +0530

    Time: 93 ms (28.32%), Space: 120.7 MB (34.72%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int n,m;
+    typedef tuple<int,int,int>t;
+    vector<pair<int,int>>direc = {{0,1},{0,-1},{1,0},{-1,0}};
+    bool isvalid(int x,int y) {
+        return x>=0&&x<n&&y>=0&&y<m;
+    }
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+        n = mat.size();
+        m = mat[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+
+        queue<t>q;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                 if(mat[i][j]==1) q.push({0,i,j});
+            }
+        }
+
+        while(!q.empty()) {
+            auto [val,x,y] = q.front();
+            q.pop();
+            for(auto [u,d]:direc) {
+                int new_x = x + u;
+                int new_y = y + d;
+                if(isvalid(new_x,new_y)&&dp[new_x][new_y]==0&&mat[new_x][new_y]!=1) {
+                    dp[new_x][new_y] = val+1;
+                    q.push({val+1,new_x,new_y});
+                }
+
+            }
+        }
+        return dp;
+    }
+};
