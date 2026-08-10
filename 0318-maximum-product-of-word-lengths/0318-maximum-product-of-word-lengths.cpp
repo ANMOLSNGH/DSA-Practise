c3d7b34ad5156fabc08c40a828659a74c1dbabab
ANMOLSNGH <101090890+ANMOLSNGH@users.noreply.github.com>
2026-08-10 22:02:51 +0530

    Time: 7 ms (66.71%), Space: 26.2 MB (46.44%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        vector<int> masks(n, 0);
+        vector<int> lengths(n, 0);
+        
+        for (int i = 0; i < n; i++) {
+            int mask = 0;
+            for (char c : words[i]) {
+                mask |= (1 << (c - 'a')); 
+            }
+            masks[i] = mask;
+            lengths[i] = words[i].length();
+        }
+        
+        int maxProd = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((masks[i] & masks[j]) == 0) {
+                    maxProd = max(maxProd, lengths[i] * lengths[j]);
+                }
+            }
+        }
+        
+        return maxProd;
+    }
+};
