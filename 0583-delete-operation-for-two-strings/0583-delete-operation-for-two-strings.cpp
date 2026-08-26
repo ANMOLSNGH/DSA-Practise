4a28341af2c3aed19c728a3657224fa02efcaa41
ANMOLSNGH <101090890+ANMOLSNGH@users.noreply.github.com>
2026-08-26 09:31:12 +0530

    Time: 15 ms (33.52%), Space: 16.8 MB (36.83%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int dp[501][501];
-    int n,m;
-    int solve(int i , int j,string &word1,string &word2) {
-     if(i>=n&&j>=m) return 0;
-     if(i>=n) return m-j;
-     if(j>=m) return n-i;
-
-     if(dp[i][j]!=-1) return dp[i][j];
-     if(word1[i]==word2[j]) {
-        return dp[i][j] = solve(i+1,j+1,word1,word2);
-     }
-     int delete1 = INT_MAX;
-     int delete2 = INT_MAX;
-
-     if(i<n)
-     delete1 = 1 + solve(i+1,j,word1,word2);
-     if(j<m)
-     delete2 = 1 + solve(i,j+1,word1,word2);
-     return dp[i][j] = min(delete1,delete2);
-
-    }
     int minDistance(string word1, string word2) {
-        n = word1.size();
-        m = word2.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,word1,word2);
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 0;i<=n;i++) dp[i][0] = i;
+
+        for(int j = 0;j<=m;j++) dp[0][j] = j;
+
+        for(int i = 1;i<=n;i++) {
+            for(int j = 1;j<= m ;j++) {
+                if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else {
+                    dp[i][j] = 1 + min(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+        
     }
 };
