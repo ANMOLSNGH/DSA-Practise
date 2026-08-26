bad685bd2304eb8e1eaa7618b71466e4ee4e8f83
ANMOLSNGH <101090890+ANMOLSNGH@users.noreply.github.com>
2026-08-26 15:58:57 +0530

    Time: 12 ms (21.22%), Space: 25.9 MB (16.57%) - LeetHub

--- 1987-number-of-unique-good-subsequences/1987-number-of-unique-good-subsequences.cpp
+++ 1987-number-of-unique-good-subsequences/1987-number-of-unique-good-subsequences.cpp
@@ -1,19 +1,32 @@
-typedef int64_t ll;
-
 class Solution {
 public:
-    int numberOfUniqueGoodSubsequences(string s) {
-        const int mod = 1e9 + 7;
-        ll dp[2][2] = {};
-        for(char c : s) {
-            int a = c - '0';
-            if(a == 0) {
-                dp[0][0] = 1;
-                dp[1][0] = (dp[1][0] + dp[1][1]) % mod;
+    int numberOfUniqueGoodSubsequences(string binary) {
+        int n = binary.size();
+        int i = 0;
+        while(i<n&&binary[i]!='1') i++;
+        if(i==n) return 1;
+
+        long long mod = 1e9 + 7;
+        vector<long long> dp(n, 0);
+        dp[i] = 1;
+        int lastZero = 0, lastOne = 0;
+        for(int indx = i+1;indx<n;indx++) {
+            int j = binary[indx]=='0'?lastZero:lastOne;
+            long long dup = (j>0)?dp[j-1]:0;
+
+            dp[indx] = (2*dp[indx-1]%mod - dup + mod)%mod;
+            if (binary[indx] == '0') {
+                lastZero = indx;
             } else {
-                dp[1][1] = (dp[1][0] + dp[1][1] + 1) % mod;
+                lastOne = indx;
             }
         }
-        return (dp[0][0] + dp[0][1] + dp[1][0] + dp[1][1]) % mod;
+
+        int hasZero = 0;
+        if (binary.find('0') != string::npos) {
+            hasZero = 1;
+        }
+
+        return (dp[n-1]+hasZero)%mod;
     }
 };
