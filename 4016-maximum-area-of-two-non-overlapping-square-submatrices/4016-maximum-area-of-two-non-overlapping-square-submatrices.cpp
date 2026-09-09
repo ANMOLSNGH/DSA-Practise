634527dfe4fc6dfa9176e7128ab2490d4a49458d
ANMOLSNGH <101090890+ANMOLSNGH@users.noreply.github.com>
2026-09-09 23:12:30 +0530

    Time: 147 ms (33.72%), Space: 162.5 MB (18.24%) - LeetHub

--- 4016-maximum-area-of-two-non-overlapping-square-submatrices/4016-maximum-area-of-two-non-overlapping-square-submatrices.cpp
+++ 4016-maximum-area-of-two-non-overlapping-square-submatrices/4016-maximum-area-of-two-non-overlapping-square-submatrices.cpp
@@ -1,69 +1,48 @@
 class Solution {
 public:
-    // make dp to solve maximum sq area ending at i,j
-
-    bool check(int n,int m,int mid , vector<vector<int>>&dp,vector<int>&suffixi,vector<int>&suffixj ) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) { 
-                if(dp[i][j]>=mid) {
-                     int rowBelow = i +mid;
-                     if(rowBelow<n&&suffixi[rowBelow]>=mid) {
-                        return true;
-                     }
-                     int colRight = j+mid;
-                     if(colRight<m&&suffixj[colRight]>=mid) return true;
-                }
-           }
-        }
-        return false;
-    }
     int maxArea(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        if(n==0||m==0) return 0;
-        vector<vector<int>>dp(n,vector<int>(m));
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                if(mat[i][j]==1) {
-                if(i==0||j==0) {
-                    dp[i][j] = 1;
+        int n = mat.size(),m = mat[0].size();
+        vector<vector<int>> dp(n,vector<int>(m));
+        unordered_map<int,vector<pair<int,int>>> mpp;
+        for(int i=0; i<n; i++){
+            dp[i][0] = (mat[i][0]==1);
+            mpp[dp[i][0]].push_back({i,0});
+        }
+        for(int j=0; j<m; j++){
+            dp[0][j] = (mat[0][j]==1);
+            mpp[dp[0][j]].push_back({0,j});
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(mat[i][j]==0) continue;
+                dp[i][j] = min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
+                mpp[dp[i][j]].push_back({i,j});
+            }
+        }
+        
+        int ans = 0;
+        for(auto& it : mpp){
+            int s = it.first;
+            vector<pair<int,int>> v = it.second;
+            int x = v.size();
+            if(x<=1) continue;
+            for(int i=0; i<x; i++){
+                for(int j=i+1; j<x; j++){
+                    int r1=v[i].first;
+                    int c1=v[i].second;
+                    int r2=v[j].first;
+                    int c2=v[j].second;
+
+                    if(abs(r1-r2)>=s || abs(c1-c2)>=s){
+                        ans=max(ans,s);
+                        break;
+                    }
                 }
-                else 
-                dp[i][j] = min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]}) + 1;
-                }
-            }
-        }
-
-        vector<int>suffixi(n);
-        vector<int>suffixj(m);
-
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                suffixi[i] = max(suffixi[i],dp[i][j]);
-                suffixj[j] = max(suffixj[j],dp[i][j]);
-            }
-        }
-
-        for(int i = n-2;i>=0;i--) {
-            suffixi[i] = max(suffixi[i],suffixi[i+1]);
-        }
-         for(int i = m-2;i>=0;i--) {
-            suffixj[i] = max(suffixj[i],suffixj[i+1]);
-        }
-
-        int low = 1;
-        int high = min(n,m);
-        int ans  = 0;
-
-        while(low<=high) {
-            int mid = low + (high - low) / 2;
-            if(check(n,m,mid,dp,suffixi,suffixj)) {
-                ans = mid;
-                low = mid + 1;
-            }
-            else high = mid-1;
+            }  
+            
         }
         return ans*ans;
 
+        
     }
 };
