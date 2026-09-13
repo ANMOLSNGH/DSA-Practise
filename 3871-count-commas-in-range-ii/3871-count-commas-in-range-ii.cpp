0e562ab2aba6c4cec87127b7cd09b4c1c1e9d7ad
ANMOLSNGH <101090890+ANMOLSNGH@users.noreply.github.com>
2026-09-13 20:15:14 +0530

    Time: 0 ms (100%), Space: 9 MB (51.75%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+   long long case1 = 1e3;
+    long long case2 = 1e6;
+    long long case3 = 1e9;
+    long long case4 = 1e12;
+    long long case5 = 1e15;
     long long countCommas(long long n) {
-        long long commas = 0;
-        long long base = 1000;
-        
-        // As long as n is greater than or equal to the current base (1e3, 1e6, 1e9...)
-        while (n >= base) {
-            commas += (n - base + 1); // Add a comma for every number >= base
-            
-            // Prevent long long overflow if n is exceptionally large
-            if (base > LLONG_MAX / 1000) break;
-            
-            base *= 1000; 
+        if(n<case1) return 0;
+        long long cnt = 0;
+        if(n>=case1&&n<case2) cnt += n-case1+1;
+        if(n>=case2&&n<case3) {
+              cnt += (case2-case1)*1 + (n-case2+1)*2;
         }
-        
-        return commas;
+        if(n>=case3&&n<case4) {
+            cnt += (case2-case1) + (case3-case2)*2 + (n-case3+1)*3;
+        }
+        if(n>=case4&&n<case5) {
+            cnt += (case2-case1) + (case3-case2)*2 + (case4-case3)*3 + (n-case4+1)*4;
+        }
+         if(n>=case5) {
+            cnt += (case2-case1) + (case3-case2)*2 + (case4-case3)*3 + (case5-case4)*4+(n-case5+1)*5;
+        }
+        return cnt;
     }
 };
